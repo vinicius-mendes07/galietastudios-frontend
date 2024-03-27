6c7180f884b080cf275d0e2028f1f753219ecac7
vinicius <vinicius.henrique.92@hotmail.com>
2024-03-27 09:21:30 -0300

    feat: create first section of home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+import { FirstSection, LinkWrapper } from './styles';
+
 export default function Home() {
   return (
-    <h1>
-      home
-    </h1>
+    <main>
+      <FirstSection id="section1">
+        <div>
+          <h2>Galieta Barbershop</h2>
+          <h1>O toque artístico que vai além do corte.</h1>
+
+          <LinkWrapper>
+            <Link to="/schedule">
+              Agendar horário
+            </Link>
+          </LinkWrapper>
+        </div>
+      </FirstSection>
+    </main>
   );
 }
