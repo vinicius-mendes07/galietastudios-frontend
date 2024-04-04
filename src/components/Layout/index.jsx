0eff07e8e7f10d4047e9ff6312bd69d0e92fbda4
vinicius <vinicius.henrique.92@hotmail.com>
2024-04-04 15:07:34 -0300

    feat: create header top

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,10 +1,27 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import Footer from '../Footer';
 import Header from '../Header';
 
 export default function Layout() {
   return (
     <>
+      <div style={{
+        backgroundColor: '#212121',
+        padding: '14px 32px',
+      }}
+      >
+        <Link
+          to="/schedule"
+          style={{
+            display: 'block',
+            margin: '0 auto',
+            color: '#fff',
+            width: 'fit-content',
+            textAlign: 'center',
+          }}
+        >🌟Agende já e receba um upgrade nas sobrancelhas grátis!
+        </Link>
+      </div>
       <Header />
       <Outlet />
       <Footer />
